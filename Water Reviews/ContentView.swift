0187bf8c0317e8ee69c74cb54dfc8e6385f20a1a
Javier <106848262+javiiicz@users.noreply.github.com>
2024-02-13 16:58:10 -0500

    Initial Commit

---
+++ Water Reviews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Water Reviews
+//
+//  Created by Javier Carrillo on 2/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Water Reviews/Water_ReviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_ReviewsApp.swift
+//  Water Reviews
+//
+//  Created by Javier Carrillo on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Water_ReviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
