9f5ff9066432772d6719da1d8a73c0d9ea327bc2
Javier <106848262+javiiicz@users.noreply.github.com>
2024-02-21 12:09:05 -0500

    Landing view Background

--- Water Reviews/ViewController/landingView.swift
+++ Water Reviews/ViewController/landingView.swift
@@ -16,10 +16,36 @@ struct landingView: View
     // View
     var body: some View
     {
-        VStack{
-            Text("Hello")
-            Text(MyVariables.email)
+        ZStack{
+            Color.blue
+                .ignoresSafeArea()
             
+            Circle()
+                .scale(0.9)
+                .foregroundColor(.white.opacity(0.15))
+                .offset(CGSize(width: -70.0, height: -200.0))
+            
+            Circle()
+                .scale(0.7)
+                .foregroundColor(.white.opacity(1))
+                .offset(CGSize(width: -70.0, height: -200.0))
+            
+            Rectangle()
+                .foregroundColor(.white.opacity(1))
+                .frame(height: 700)
+                .offset(CGSize(width: 0, height: 100))
+            
+            VStack{
+                Text("Hello")
+                Text(MyVariables.email)
+                
+            }
         }
+        
+        
     }
+}
+
+#Preview {
+    landingView()
 }
--- Water Reviews/ViewController/mainView.swift
+++ Water Reviews/ViewController/mainView.swift
@@ -24,6 +24,3 @@ struct mainView: View {
     }
 }
 
-#Preview {
-    mainView()
-}
