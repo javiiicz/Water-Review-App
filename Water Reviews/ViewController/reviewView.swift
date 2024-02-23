2409d680c37d0e86b11a0ec1c12ce3c2c64867c7
Javier <106848262+javiiicz@users.noreply.github.com>
2024-02-23 13:02:03 -0500

    Crete and implement logic for switching views

---
+++ Water Reviews/ViewController/reviewView.swift
@@ -0,0 +1,18 @@
+//
+//  reviewView.swift
+//  Water Reviews
+//
+//  Created by Javier Carrillo on 2/23/24.
+//
+
+import SwiftUI
+
+struct reviewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    reviewView()
+}
