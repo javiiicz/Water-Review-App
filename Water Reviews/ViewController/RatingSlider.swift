4475da7571e9308edb6a0d3e2f24214b7c331912
Javier <106848262+javiiicz@users.noreply.github.com>
2024-03-01 13:49:31 -0500

    Extract Slider View

---
+++ Water Reviews/ViewController/RatingSlider.swift
@@ -0,0 +1,18 @@
+//
+//  RatingSlider.swift
+//  Water Reviews
+//
+//  Created by Javier Carrillo on 3/1/24.
+//
+
+import SwiftUI
+
+struct RatingSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingSlider()
+}
