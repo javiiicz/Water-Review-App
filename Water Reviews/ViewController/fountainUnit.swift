80c47767cc1025df12cd422b39fef057f3b87e1f
Javier <106848262+javiiicz@users.noreply.github.com>
2024-03-03 23:09:21 -0500

    Rename file to ratingSlider

---
+++ Water Reviews/ViewController/fountainUnit.swift
@@ -0,0 +1,18 @@
+//
+//  fountainUnit.swift
+//  Water Reviews
+//
+//  Created by Javier Carrillo on 3/3/24.
+//
+
+import SwiftUI
+
+struct fountainUnit: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    fountainUnit()
+}
